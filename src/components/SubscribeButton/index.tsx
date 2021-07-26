8d4f8e32a41aba32adc720ba6c483206d3168d99
Daniel Nunez <daniel.nunez@nave.rs>
2021-07-26 18:43:56 -0300

    feat: add home page

---
+++ src/components/SubscribeButton/index.tsx
@@ -0,0 +1,15 @@
+import styles from './styles.module.scss'
+
+const SubscribeButton = () => {
+	return (
+		<button
+			type="button"
+			className={styles.subButton}
+		>
+			Subscribe now
+		</button>
+	)
+}
+
+
+export { SubscribeButton }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,29 @@
 import Head from 'next/head'
 
+import { SubscribeButton } from 'components'
+
+import styles from 'styles/home.module.scss'
+
 export default function Home() {
   return (
 		<>
 			<Head>
 				<title>Home | ig news</title>
 			</Head>
-			<h1>Lorem ipsum dolor sit amet, consectetur adipis</h1>
+			<main className={styles.container}>
+				<section className={styles.hero}>
+					<span className={styles.subtitle}>👏 Hey, welcome</span>
+					<h1>
+						News about the <span>React</span> world
+					</h1>
+					<p>
+						Get acess to all the publications<br />
+						<span>for $9.90 month</span>
+					</p>
+					<SubscribeButton />
+				</section>
+				<img src="/images/avatar.svg" alt="girl-coding" />
+			</main>
 		</>
   )
 }
